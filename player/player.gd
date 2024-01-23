bf267d691ca6f900ce12b332c70c7783542d3d9c
Brendan Kersey <techsupport2@wildsidesmoke.com>
2024-01-23 01:45:14 -0500

    changed from nuggets to elemental dudes, changed movement to feel good, working on animations and h flipping

--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,32 @@
 extends CharacterBody2D
 
-var player_speed = 200
+const SPEED = 300
+const ACCEL = 10.0
 
-func _physics_process(delta):
-	var direction = Input.get_vector("move_left","move_right","move_up", "move_down")
-	velocity = direction * player_speed
-	if velocity != Vector2(0,0):
-		$AnimatedSprite2D.play("walk")
-		move_and_slide()
+var input: Vector2
+var is_player_facing_right = true
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+func get_input():
+	input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return input.normalized()
+
+func update_animations(inputs):
+	if inputs == Vector2(0,0):
+		animationPlayer.play("idle")
 	else:
-		$AnimatedSprite2D.play("idle")
+		animationPlayer.play("run")
+
+func _process(delta):
+	var playerInput = get_input()
+	velocity = lerp(velocity, playerInput * SPEED, delta*ACCEL)
+	if playerInput.x !=0:
+		sprite.flip_h = !sprite.flip_h
+	move_and_slide()
+	
+	update_animations(playerInput)
+func _physics_process(_delta):
+	pass
