ef340e44c479da43e3d8e483ea134300be6a078f
Brendan Kersey <techsupport2@wildsidesmoke.com>
2024-01-29 01:23:13 -0500

    Made a simple map. Next will be collisions and attacks

--- player/player.gd
+++ player/player.gd
@@ -8,25 +8,33 @@ var is_player_facing_right = true
 
 @onready var animationPlayer = $AnimationPlayer
 @onready var sprite = $Sprite2D
-
+@onready var animationTree = $AnimationTree
 func get_input():
 	input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return input.normalized()
 
 func update_animations(inputs):
-	if inputs == Vector2(0,0):
-		animationPlayer.play("idle")
-	else:
-		animationPlayer.play("run")
-
-func _process(delta):
+	print(inputs)
+	animationTree.set("parameters/Move/blend_position", inputs)
+func _ready():
+	animationTree.active = true
+#func _process(delta):
+	#var playerInput = get_input()
+	#velocity = lerp(velocity, playerInput * SPEED, delta*ACCEL)
+	#if playerInput.x !=0:
+		#sprite.flip_h = !sprite.flip_h
+	#move_and_slide()
+	
+func _physics_process(delta):
 	var playerInput = get_input()
+	
 	velocity = lerp(velocity, playerInput * SPEED, delta*ACCEL)
 	if playerInput.x !=0:
-		sprite.flip_h = !sprite.flip_h
+		if playerInput.x < 0 && !sprite.flip_h:
+			sprite.flip_h = true
+		if playerInput.x > 0 && sprite.flip_h:
+			sprite.flip_h = false
 	move_and_slide()
-	
+
 	update_animations(playerInput)
-func _physics_process(_delta):
-	pass
